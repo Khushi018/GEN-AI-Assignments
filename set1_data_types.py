ee07178990c17dda1c28f6dcd3888644a1541416
Khushi Gupta <khushigupta.of@gmail.com>
2023-07-08 18:03:22 +0530

    Create set1_data_types.py

---
+++ set1_data_types.py
@@ -0,0 +1,14 @@
+set1_data_types
+
+print("hello world");
+i = 2
+f = 2.0;
+s = "Khushi";
+b = True;
+list  = [2,3,4,5,6]
+dict = {"khushi":1, "gupta" : 3};
+touple = (3,5,6,7,8);
+set = {2 ,3,4,2,3,4};
+
+
+print(type(i), type(f), type(s), type(b), type(list), type(dict), type(touple), type(set));

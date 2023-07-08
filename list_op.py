e5facb4963bdff63623af5befe5c5f2c8a0b4bcb
Khushi Gupta <khushigupta.of@gmail.com>
2023-07-08 18:05:41 +0530

    Create list_op.py

---
+++ list_op.py
@@ -0,0 +1,16 @@
+my_list = []
+
+
+my_list.append(5)
+my_list.append(2)
+my_list.append(8)
+my_list.append(1)
+
+my_list.remove(2)
+my_list.remove(8)
+
+
+print("List after removing elements:", my_list)
+
+# Sort the list using sort()
+my_list.sort()
